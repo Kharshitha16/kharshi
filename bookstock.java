aed36543de9144389c72ad61554a7a9f0312de59
Kharshitha16 <69036407+Kharshitha16@users.noreply.github.com>
2020-10-31 23:01:32 +0530

    Add files via upload

---
+++ bookstock.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author admin
+ */
+public class bookstock {
+    String title;
+    String author;
+    String publisher;
+    double price;
+    int stock;
+     bookstock(String t,String a,String p,double pri,int s){
+     title=t;
+     author=a;
+     publisher=p;
+     price=pri;
+     stock=s;
+     
+     }
+     
+    
+    
+}
